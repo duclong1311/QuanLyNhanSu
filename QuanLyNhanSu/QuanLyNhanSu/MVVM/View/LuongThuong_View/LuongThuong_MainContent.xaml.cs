587a3d5da1f790f000760a38fc0ce00051eda995
Duc Long <duclong1311.work@gmail.com>
2024-04-12 16:16:04 +0700

    BaseView
    View layout for project

---
+++ QuanLyNhanSu/QuanLyNhanSu/MVVM/View/LuongThuong_View/LuongThuong_MainContent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyNhanSu.MVVM.View.LuongThuong_View
+{
+    /// <summary>
+    /// Interaction logic for LuongThuong_MainContent.xaml
+    /// </summary>
+    public partial class LuongThuong_MainContent : UserControl
+    {
+        public LuongThuong_MainContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/MVVM/View/TinhCong_View/TinhCong_MainContent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyNhanSu.MVVM.View.TinhCong_View
+{
+    /// <summary>
+    /// Interaction logic for TinhCong_MainContent.xaml
+    /// </summary>
+    public partial class TinhCong_MainContent : UserControl
+    {
+        public TinhCong_MainContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
